40f75487a40bc6d0bba7441b8df4a57dec8ccbbf
Tilak N Shenoy <tilaknshenoy@gmail.com>
2018-04-10 23:46:36 +0530

    UDP FTP

--- TCP FTP/server.c
+++ TCP FTP/server.c
@@ -16,7 +16,9 @@ int main(int argc, char const *argv[])
 	struct sockaddr_in servaddr,cliaddr;
 	int listenfd,connfd,clilen;
 	listenfd=socket(AF_INET,SOCK_STREAM,0);
+
 	bzero(&servaddr, sizeof(servaddr));
+	
 	servaddr.sin_family=AF_INET;
 	servaddr.sin_port=htons(SERV_PORT);
 
